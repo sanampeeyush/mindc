dbebf4d9e1b175cdf172ff3903f80a81371af930
Peeyush Sanam <peeyush@exackt.com>
2023-09-20 17:09:05 +0530

    ssiddata

--- basicuser/views.py
+++ basicuser/views.py
@@ -9,10 +9,20 @@ def create_ssid_data(request):
     if request.method == 'POST':
         form = SSIDDataForm(request.POST)
         if form.is_valid():
+            mac_address = form.cleaned_data['mac']
+            
+            # Check if a record with the same MAC address already exists
+            existing_record = models.SSIDData.objects.filter(mac=mac_address).first()
+            if existing_record:
+                # Handle the case where the MAC address already exists
+                return render(request, 'duplicate_mac_error.html')
+            
+            # If no existing record found, save the new record
             form.save()
-            return redirect('home_page')  # Redirect to a success page after form submission
+            return redirect('success')  # Redirect to a success page after form submission
     else:
         form = SSIDDataForm()
+    
     return render(request, 'basicuser/create_ssid_data.html', {'form': form})
 
 def wifi_credential_by_mac(request, mac):
