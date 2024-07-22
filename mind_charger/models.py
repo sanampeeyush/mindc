16476d7dd228f479169008db75c461392ea7dcbc
sanampeeyush <peeyush.sanam@gmail.com>
2024-07-22 15:57:36 +0530

    change model

--- mind_charger/models.py
+++ mind_charger/models.py
@@ -1,9 +1,37 @@
 from django.db import models
+import os
+
+
+def upload_location(self, _):
+    return f"audio_files/{self.date.strftime('%d-%m-%Y')}-{self.button_id}.mp3"
+
 
 class AudioFile(models.Model):
-    title = models.CharField(max_length=255)
-    audio = models.FileField(upload_to='audio_files/')
+    date = models.DateField()
+    button_id = models.CharField(
+        choices=[
+            ("01", "01"),
+            ("02", "02"),
+            ("03", "03"),
+            ("04", "04"),
+            ("05", "05"),
+            ("06", "06"),
+            ("07", "07"),
+            ("08", "08"),
+            ("09", "09"),
+            ("10", "10"),
+        ],
+        max_length=2,
+        default="01",
+    )
+    audio = models.FileField(upload_to=upload_location)
     uploaded_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f"{str(self.date)}-{self.button_id}"
+
+    def delete(self, *args, **kwargs):
+        if self.audio:
+            if os.path.isfile(self.audio.path):
+                os.remove(self.audio.path)
+        super(AudioFile, self).delete(*args, **kwargs)
--- mind_charger/views.py
+++ mind_charger/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+
 def home_page(req):
-    return render(req,'home/index.html')
+    return render(req, "home/index.html")
